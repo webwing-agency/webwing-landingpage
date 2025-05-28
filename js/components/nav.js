99fc2876d5285513856b601fb34000d90c965d0c
paul <paul@webwing.agency>
2025-05-28 23:37:53 +0200

    hide "index" domains for site aesthetics

--- js/components/nav.js
+++ js/components/nav.js
@@ -13,11 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="popup-menu">
             <div class="popup-menu-card">
                 <div class="main-link-wrapper">
-                    <a class='main-link link_animated' href='indexxx'>Home</a>
-                    <a class='main-link link_animated' href='servicesxx'>Services</a>
-                    <a class='main-link link_animated' href='workxx'>Work</a>
-                    <a class='main-link link_animated' href='about-usxx'>About Us</a>   
-                    <a class='main-link link_animated' href='teamxx'>Team</a>  
+                    <a class='main-link link_animated' href='/'>Home</a>
+                    <a class='main-link link_animated' href='services'>Services</a>
+                    <a class='main-link link_animated' href='work'>Work</a>
+                    <a class='main-link link_animated' href='about-us'>About Us</a>   
+                    <a class='main-link link_animated' href='team'>Team</a>  
                 </div>
             </div>
 
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             <div class="secondary-cta-wrapper">
                 <div class="secondary-cta">
-                  <a href="contactxx" class="main-link">Contact</a>
+                  <a href="contact" class="main-link">Contact</a>
                 </div>
             </div>
             </div>
@@ -202,7 +202,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const navLogo = document.querySelector(".nav-logo");
 
   navLogo.addEventListener("click", () => {
-    window.location.href = "indexxx"; // Navigate to indexxx
+    window.location.href = "/"; 
   });
 
   const links = document.querySelectorAll("a");
