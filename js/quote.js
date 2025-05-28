ea3377c8f69e2d4b10929ed30d05f11dee8f9daa
paul <paul@webwing.agency>
2025-05-28 18:36:59 +0200

    misc fixes

--- js/quote.js
+++ js/quote.js
@@ -89,6 +89,28 @@ prevBtn.addEventListener('click', () => {
   }
 });
 
+
+// ------------------------------
+// MAIL VALIDATION
+// ------------------------------
+function setupEmailValidation() {
+  const emailInput = document.querySelector('input[name="email"]');
+  if (!emailInput) return;
+
+  emailInput.addEventListener('input', () => {
+    const value = emailInput.value;
+    const [local] = value.split('@');
+    const digits = (local.match(/\d/g) || []).length;
+
+    if (digits > 0) {
+      emailInput.setCustomValidity('Please provide a professional email address');
+    } else {
+      emailInput.setCustomValidity('');
+    }
+  });
+}
+
+
 // ------------------------------
 // URL PARAM STEP SKIPPER
 // ------------------------------
