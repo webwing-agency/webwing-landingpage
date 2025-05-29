f1b9449cea7f00464e832c52b13d67d9badd3ea4
paul <paul@webwing.agency>
2025-05-29 12:12:33 +0200

    update cta component script to feature correct arrow icon classifications for correct styling

--- js/components/cta.js
+++ js/components/cta.js
@@ -23,9 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
         <span class="gradient-bg"></span>
         <span class="get-a-quote-button-content">
             <span>Get a Quote</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-            </svg>
+            <div class="arrow-container">
+                <svg class="arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                </svg>
+            </div>
         </span>
     `;
 
